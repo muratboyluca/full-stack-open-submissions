3176513f07b69742d3beb26d403557c4a9b19f39
Murat Boyluca <muratboyluca@gmail.com>
2022-02-06 13:22:16 +0100

    exercise 1.11 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,28 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
-const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+const Button = ({ type, onClick }) => {
+  return (
+    <button type="button" onClick={onClick}>
+      {type}
+    </button>
+  );
+};
 
+const Description = ({ type, count }) => {
+  return (
+    <tr>
+      <td>{type}</td>
+      <td>{count}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad, total }) => {
+  if (total === 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No feedback yet - be the first!</p>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+          <Description type="good" count={good} />
+          <Description type="neutral" count={neutral} />
+          <Description type="bad" count={bad} />
+          <Description type="all" count={total} />
+          <Description type="average" count={(good - bad) / total} />
+          <Description type="positive" count={(good / total) * 100 + "%"} />
+        </tbody>
+      </table>
     </div>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const total = good + neutral + bad;
+
+  const handleGood = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <main>
+      <h1>give feedback</h1>
+      <Button type="good" onClick={handleGood} />
+      <Button type="neutral" onClick={handleNeutral} />
+      <Button type="bad" onClick={handleBad} />
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} />
+    </main>
   );
 };
 
